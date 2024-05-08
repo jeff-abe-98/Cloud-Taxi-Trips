8927ee70bdb4af1d562eda43537ef78bee582a91
jeff-abe-98 <jeffreymarshallabraham@gmail.com>
2024-05-08 17:59:51 -0500

    adding second function

--- function_app.py
+++ function_app.py
@@ -3,13 +3,16 @@ import azure.functions as func
 from requests.auth import HTTPBasicAuth
 import requests
 import os
+from io import StringIO
+from stream_unzip import stream_unzip
 from azure.storage.blob import BlobClient
- 
+
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 0 1 1 *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+
+@app.schedule(schedule="0 0 0 1 1 *", arg_name="myTimer",
+              run_on_startup=True, use_monitor=False)
 def zones_pull(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('Starting timer triggered function')
@@ -24,9 +27,55 @@ def zones_pull(myTimer: func.TimerRequest) -> None:
         url = 'https://data.cityofnewyork.us/resource/755u-8jsi.csv'
 
         rsp = requests.request('get', url, auth=nyc_open_auth)
-
-        blob = BlobClient(account_url='https://oecapstorage.blob.core.windows.net', container_name=r'raw', blob_name='taxi_zones.csv', credential=storage_token)
-        logging.info(f'Writing csv file in /raw/taxi_zones.csv')
+        location = 'https://oecapstorage.blob.core.windows.net'
+        blob = BlobClient(account_url=location,
+                          container_name=r'raw',
+                          blob_name='taxi_zones.csv',
+                          credential=storage_token)
+        logging.info('Writing csv file in /raw/taxi_zones.csv')
         blob.uploadBlob(rsp.content.decode('utf-8'))
+
+    logging.info('Python timer trigger function executed.')
+
+
+@app.schedule(schedule="0 0 0 1 1 *", arg_name="myTimer",
+              run_on_startup=True, use_monitor=False)
+def bike_pull(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('Process Started')
+        storage_token = os.environ['blob_storage_token']
+
+        def yield_chunks(year):
+            s = requests.Session()
+            url = f'https://s3.amazonaws.com/tripdata/{year}-citibike-tripdata.zip' # noqa
+            with s.request('get', url,
+                           stream=True) as rsp:
+                yield from rsp.iter_content(chunk_size=262144)
+
+        files_names = []
+        for year in [2014]:
+            for file_name, _, chunk in stream_unzip(yield_chunks(year)):
+                str_file = file_name.decode('utf-8')
+
+                if '.csv' not in str_file or 'MAC' in str_file:
+                    for _ in chunk:
+                        pass
+                    continue
+                elif str_file in files_names:
+                    pass
+                else:
+                    files_names.append(str_file)
+                    logging.info(f'Beginning extraction of {str_file}')
+                with StringIO() as file:
+                    for chunk in chunk:
+                        file.write(chunk.decode())
+                    file.seek(0)
+                    location = 'https://oecapstorage.blob.core.windows.net'
+                    blob = BlobClient(account_url=location,
+                                      container_name=r'raw',
+                                      blob_name=str_file,
+                                      credential=storage_token)
+                    logging.info('Writing csv file in /raw/taxi_zones.csv')
+                    blob.uploadBlob(file)
 
     logging.info('Python timer trigger function executed.')
